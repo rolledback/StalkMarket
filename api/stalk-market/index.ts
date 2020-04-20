e0c66de0fde1461914a3d5f390145f98c5f79f83
Matthew Rayermann <matthew.rayermann@gmail.com>
2020-04-19 22:12:07 -0700

    Check in Azure function

---
+++ api/stalk-market/index.ts
@@ -0,0 +1,47 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { PriceAnalysis, analyzePrices } from "stalk-market";
+import * as crypto from "crypto";
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    let log: string[] = [];
+    let start = Date.now();
+    let response: { result?: PriceAnalysis; requestId: string; } = { requestId: newRequestId() };
+
+    try {
+        log.push(`Request: ${JSON.stringify(req)}`);
+        let knownPrices: number[] = req.body.knownPrices;
+        let previousPattern: number | undefined = req.body.previousPattern;
+
+        log.push(`Known prices: ${JSON.stringify(knownPrices)}`);
+        log.push(`Previous pattern: ${JSON.stringify(previousPattern)}`);
+
+        for (let i = 0; i < 14; i++) {
+            if (knownPrices[i] === undefined) {
+                knownPrices[i] = 0;
+            }
+        }
+
+        response.result = analyzePrices(knownPrices, previousPattern);
+    } catch (err) {
+        log.push(`Error: ${err.toString()}`);
+    }
+
+    context.res = {
+        body: response,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+
+    let end = Date.now();
+    log.push(`Execution time: ${end - start}ms`);
+
+    context.log(response);
+    context.done();
+};
+
+function newRequestId(): string {
+    return crypto.randomBytes(16).toString('hex');
+}
+
+export default httpTrigger;
